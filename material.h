62140ee8c6765c9a58dfd9bde0c84765812e99ad
Jun Kamoshima <kamoshima@gmail.com>
2018-03-22 23:03:31 +0900

    added fuzziness

--- material.h
+++ material.h
@@ -36,12 +36,13 @@ vec3 reflect(const vec3& v, const vec3& n)
 
 class metal : public material {
 public:
-    metal(const vec3& a) : albedo(a) {}
+    metal(const vec3& a, float f) : albedo(a), fuzz(f) {}
     bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
     vec3 albedo;
+    float fuzz;
 };
