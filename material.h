307ecebee1d19b9a4e4bd66c289f3d93e68af74d
Jun Kamoshima <kamoshima@gmail.com>
2018-03-23 08:35:45 +0900

    added refractive material (dielectric)

--- material.h
+++ material.h
@@ -36,7 +36,7 @@ vec3 reflect(const vec3& v, const vec3& n)
 
 class metal : public material {
 public:
-    metal(const vec3& a, float f) : albedo(a), fuzz(f) {}
+    metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
     bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
         scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
@@ -45,4 +45,51 @@ public:
     }
     vec3 albedo;
     float fuzz;
+};
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
+{
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt * dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+float schlick(float cosine, float ref_idx)
+{
+    float r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
+class dielectric : public material {
+public:
+    dielectric(float ri) : ref_idx(ri) {}
+    bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const override {
+        vec3 outward_normal;
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        float ni_over_nt;
+        attenuation = vec3(1.0, 1.0, 0.0);
+        vec3 refracted;
+        if (dot(r_in.direction(), rec.normal) > 0) {
+            outward_normal = -rec.normal;
+            ni_over_nt = ref_idx;
+        } else {
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0 / ref_idx;
+        }
+        if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+            scattered = ray(rec.p, refracted);
+        } else {
+            scattered = ray(rec.p, reflected);
+            return false;
+        }
+        return true;
+    }
+    float ref_idx;
 };
