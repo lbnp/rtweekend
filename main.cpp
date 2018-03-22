89b416f3f5110e1ea14f1a9705217e8eaf6f135d
Jun Kamoshima <kamoshima@gmail.com>
2018-03-22 22:58:30 +0900

    material implementation

--- main.cpp
+++ main.cpp
@@ -4,24 +4,20 @@
 #include "sphere.h"
 #include "hittable_list.h"
 #include "camera.h"
+#include "material.h"
 #include <cfloat>
 
-vec3 random_in_unit_sphere()
-{
-    vec3 p;
-    do {
-        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1.0, 1.0, 1.0);
-    } while (p.squared_length() >= 1.0);
-
-    return p;
-}
-
-vec3 color(const ray& r, hittable* world)
+vec3 color(const ray& r, hittable* world, int depth)
 {
     hit_record rec;
     if (world->hit(r, 0.001, FLT_MAX, rec)) {
-        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * color(ray(rec.p, target - rec.p), world);
+        ray scattered;
+        vec3 attenuated;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuated, scattered)) {
+            return attenuated * color(scattered, world, depth + 1);
+        } else {
+            return vec3(0, 0, 0);
+        }
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
@@ -34,10 +30,12 @@ int main()
     int ny = 100;
     int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    hittable* list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
-    hittable* world = new hittable_list(list, 2);
+    hittable* list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+    hittable* world = new hittable_list(list, 4);
     camera cam;
 
     for (int j = ny - 1; j >= 0; j--) {
@@ -47,7 +45,7 @@ int main()
                 float u = float(i + drand48()) / float(nx);
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= float(ns);
             col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
--- sphere.h
+++ sphere.h
@@ -4,10 +4,11 @@
 class sphere : public hittable {
 public:
     sphere() {}
-    sphere(vec3 cen, float r) : center(cen), radius(r) {}
+    sphere(vec3 cen, float r, material* mat) : center(cen), radius(r), mat_ptr(mat) {}
     bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const override;
     vec3 center;
     float radius;
+    material* mat_ptr;
 };
 
 bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const
@@ -23,6 +24,7 @@ bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
             rec.normal = (rec.p - center) / radius;
+            rec.mat_ptr = mat_ptr;
             return true;
         }
         temp = (-b + sqrt(b * b - a * c)) / a;
@@ -30,6 +32,7 @@ bool sphere::hit(const ray& r, float tmin, float tmax, hit_record& rec) const
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
             rec.normal = (rec.p - center) / radius;
+            rec.mat_ptr = mat_ptr;
             return true;
         }
     }
